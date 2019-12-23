efc843d5967ce385897edd94078933e7ab479d94
Kate0516 <gaozhen0516@gmail.com>
2019-12-23 14:00:43 +0800

    hom10

---
+++ homework10/Assets/butt.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class butt : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbs;
+    public Animator animator;
+
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = vbs.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Pressed");
+        animator.SetBool("isjump", true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Released");
+        animator.SetBool("isjump", false);
+    }
+}
